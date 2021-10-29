bbb400ac04db6f64cbf4e401b02095959df168ba
Daminichandrakar <damini.2201@gmail.com>
2021-10-29 17:52:18 +0530

    Added: giving different values feet obj should not be equals

--- src/test/java/com/quantitymesurment/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymesurment/QuantityMeasurementTest.java
@@ -6,9 +6,16 @@ import org.junit.Test;
 public class QuantityMeasurementTest {
 
 	@Test
-	 public void givenValueInFeet_WhenEqual_ShouldReturnTrue() {
+	 public void givenValueInFeet_WhenEqual_ShouldReturnEquals() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1, feet2);
     }
+	
+	@Test
+	 public void givenValueIn0FeetAnd1Feet_WhenNotEqual_ShouldReturnNotEquals() {
+       Feet feet1 = new Feet(0.0);
+       Feet feet2 = new Feet(1.0);
+       Assert.assertNotEquals(feet1, feet2);
+   }
 }
