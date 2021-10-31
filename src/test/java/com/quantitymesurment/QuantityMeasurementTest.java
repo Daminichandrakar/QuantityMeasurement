0a9e63b3a31ae4a7262b7518d13e0717f847fb41
Daminichandrakar <damini.2201@gmail.com>
2021-10-31 23:50:16 +0530

    Added: test case to check when give 0 centimeter and null return notequal

--- src/test/java/com/quantitymesurment/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymesurment/QuantityMeasurementTest.java
@@ -213,5 +213,12 @@ public class QuantityMeasurementTest {
 		QuantityMeasurement centimeter2 = new QuantityMeasurement(LengthComparision.CENTIMETER, 1.0);
 		Assert.assertNotEquals(centimeter1, centimeter2);
 	}
+	
+	@Test
+	public void given0CentimeterAndNull_WhenCompare_ShouldNotEqual() {
+		QuantityMeasurement centimeter1 = new QuantityMeasurement(LengthComparision.CENTIMETER, 0.0);
+		QuantityMeasurement centimeter2 = null;
+		Assert.assertNotEquals(centimeter1, centimeter2);
+	}
 
 }
